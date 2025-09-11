779437b1506815e4a7b956ceec5c1b9c68b59e41
Abdullah Alrashdi <alrashdi0002001@gmail.com>
2025-09-11 15:57:50 +0400

    #TRA-5319: Task 5_Power Calculation exp Is Done

---
+++ TaskFive.java
@@ -0,0 +1,20 @@
+public class TaskFive {
+        public static void main(String[] args) {
+            int base = 3;
+            int exponent = 4;
+            long result = powerRecursive(base,exponent);
+            System.out.println("Calculating " + base + "^" + exponent + " using recursion...");
+            System.out.println("Result: " + result); // Expected: 81
+        }
+
+
+        public static long powerRecursive(int base, int exp) {
+
+            if (exp == 0) {
+                return 1;
+            }
+
+            return base * powerRecursive(base, exp - 1);
+        }
+}
+
