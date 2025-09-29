0ac8455f181f619d37826526b120a413032d5a5d
Abdullah Alrashdi <alrashdi0002001@gmail.com>
2025-09-29 11:49:58 +0400

    #TRA-5351 : Task8_SumofDigitsofaNumber is Done

---
+++ TaskEight.java
@@ -0,0 +1,22 @@
+public class TaskEight {
+        public static void main(String[] args) {
+            int number = 12345;
+            int sum = 0;
+            System.out.println("Calculating the sum of digits for " + number + " with recursion...");
+
+            sum = sumDigitsRecursive(number);
+
+            System.out.println("The sum of the digits is: " + sum);
+        }
+
+
+        public static int sumDigitsRecursive(int number) {
+
+            if (number == 0) {
+                return 0;
+            }
+
+            return   (number % 10) + sumDigitsRecursive(number / 10);
+        }
+    }
+
